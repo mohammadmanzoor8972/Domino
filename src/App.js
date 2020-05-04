4e46d6fb953d2db3b7a305f4c078dfa34e177234
mohammad.manzoor8972@gmail.com <malam@LKNDSS0NRG3QD.local>
2020-05-05 01:07:45 +0530

    Added fonts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    color: ${props => (props.whiteColor ? 'white' : 'black')};
+    background-color: yellow;
+    font-family : 'Open Sans', sans-sarif;
+  },
+
+  h1,h2,h3,h4 {
+    font-family: 'Righteous', cursive;
+  }
+`
 
 function App() {
   return (
+    <>
+    <GlobalStyle/>
     <div>
-        Hello Domino
+        <h1>Hello Domino</h1>
+        test
     </div>
+        </>
   );
 }
 
