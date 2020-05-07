e1b0c14b57852ca5bbde7d274ec614e5b47bf789
mohammad.manzoor8972@gmail.com <malam@LKNDSS0NRG3QD.local>
2020-05-08 00:07:11 +0530

    Added Orders and  Openfoods hooks

---
+++ src/Hooks/useOpenFood.js
@@ -0,0 +1,11 @@
+import {useState } from 'react';   
+
+
+export function useOpenFood(){
+    const [openFood, setOpenFood] = useState();
+
+    return {
+        openFood, setOpenFood
+    }
+
+}
---
+++ src/Hooks/useOrders.js
@@ -0,0 +1,11 @@
+import {useState } from 'react';   
+
+
+export function useOrders(){
+    const [orders, setOrders] = useState([]);
+
+    return {
+        orders, setOrders
+    }
+
+}
